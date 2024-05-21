1ea8973fffdef8cbc6828ddfa301feed07b3ce58
CamiloCrd <dcamilocg@gmail.com>
2024-05-20 21:51:17 -0600

    Confirmo

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola desde CDMX el 20 de mayo a las 9pm");
+    expect(hello.hello()).toEqual("Hola desde CDMX el 20 de Mayo a las 9pm");
   });
 });
