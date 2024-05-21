feda341ba3488785ec216a7922b77b8d4e0bb57d
CamiloCrd <dcamilocg@gmail.com>
2024-05-20 21:44:41 -0600

    Confirmo

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hola desde CDMX el 20 de Mayo a las 9pm";
 console.log(hello());
 exports.hello = hello;
 
